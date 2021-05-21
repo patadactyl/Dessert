a9dc31a418be87d0d20d3847f4bf10472a6fb9f6
Patrick Charlton <37526417+patadactyl@users.noreply.github.com>
2021-05-21 15:46:50 -0700

    Made DessertTimer implement LifecycleObserver. Passed MainActivity's lifecycle to DessertTimer.kt Remove startTimer and stopTimer calls from onStart and onStop in MainActivity

--- app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
+++ app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
@@ -37,7 +37,7 @@ import timber.log.Timber
  *
  */
 class DessertTimer(lifecycle: Lifecycle) : LifecycleObserver {
-    
+
     // The number of seconds counted since the timer started
     var secondsCount = 0
 
